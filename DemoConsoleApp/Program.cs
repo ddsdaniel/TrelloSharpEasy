cf3cfdd108c7432b9437e3722c48d9c069699bf0
Daniel Dorneles da Silva <dds.daniel@gmail.com>
2020-02-15 18:43:10 -0300

    orcamento

--- DemoConsoleApp/Program.cs
+++ DemoConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 using TrelloSharpEasy.Services;
 
 namespace DemoConsoleApp
@@ -7,32 +9,56 @@ namespace DemoConsoleApp
     {
         static void Main(string[] args)
         {
-            var appKey = "";
-            var userToken = "";
+            var appKey = Environment.GetEnvironmentVariable("TRELLO_SHARP_EASY_APP_KEY", EnvironmentVariableTarget.User);
+            var userToken = Environment.GetEnvironmentVariable("TRELLO_SHARP_EASY_USER_TOKEN", EnvironmentVariableTarget.User);
+            var boardId = Environment.GetEnvironmentVariable("TRELLO_SHARP_EASY_BOARD_ID", EnvironmentVariableTarget.User);
+            var hourlyRate = Convert.ToDecimal(Environment.GetEnvironmentVariable("TRELLO_SHARP_EASY_HOURLY_RATE", EnvironmentVariableTarget.User));
 
             var easyService = new EasyService(appKey, userToken);
 
-            var board = easyService.GetBoard("");
+            var board = easyService.GetBoard(boardId);
 
             var list = board.Lists.Find(l => l.Name.Equals("Aguardando Aprovação"));
             var sb = new StringBuilder();
+            var regex = new Regex(@"\(\d\d:\d\d\)");
 
             foreach (var card in list.Cards)
             {
                 sb.AppendLine(card.Name);
 
-                foreach (var checkList in card.CheckLists)
+                var analise = card.CheckLists.Find(chk => chk.Name.Equals("Análise"));
+                if (analise != null)
                 {
-                    foreach (var item in checkList.Items)
+                    foreach (var item in analise.Items)
                     {
-                        sb.AppendLine($"- {item.Name}");
+                        var hour = regex.Match(item.Name).Value;
+                        if (String.IsNullOrEmpty(hour))
+                            throw new Exception(item.Name + " sem não estimado.");
+
+                        var name = item.Name.Replace(hour, "");
+                        hour = hour.Replace("(", "").Replace(")", "");
+
+                        var price = hourlyRate * ConvertHourToDecimal(hour);
+
+                        sb.AppendLine($"'-   {name}\t{hour}\t{price.ToString("#,##0.00")}");
                     }
                 }
 
-                sb.AppendLine();
             }
 
             var tasks = sb.ToString();
+        }
+
+        private static decimal ConvertHourToDecimal(string hhmm)
+        {
+            var array = hhmm.Split(':');
+
+            var hours = Convert.ToDecimal(array[0]);
+            var minutes = Convert.ToDecimal(array[1]);
+
+            minutes = Math.Round(minutes / 60, 2, MidpointRounding.AwayFromZero);
+
+            return hours + minutes;
         }
     }
 }
